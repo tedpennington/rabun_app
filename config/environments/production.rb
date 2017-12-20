047263a97bc089b6c1962ce25e07786e5800511d
Ted Pennington <tedpennington@Teds-MacBook-Pro.local>
2017-12-20 14:57:17 -0600

    Complete Devise Gem install first stage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Required for Devise Gem.  Remember to change localhost:3000 to actual application host (custom domain) -tp
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
