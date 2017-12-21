6d4e43af60331aea3344c90c5d016ec682b36995
Ted Pennington <tedpennington@Teds-MacBook-Pro.local>
2017-12-21 12:10:49 -0600

    set up links and routing for renters

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -32,7 +32,7 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to renter_reviews_path, notice: 'Review was successfully created.' }
+        format.html { redirect_to renter_path(@renter), notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
