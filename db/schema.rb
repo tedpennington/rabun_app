dab0cea99171a65d746e901ae5e99c467decddbe
Ted Pennington <tedpennington@Teds-MacBook-Pro.local>
2017-12-20 20:03:39 -0600

    Link reviews to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171221013801) do
+ActiveRecord::Schema.define(version: 20171221014951) do
 
   create_table "renters", force: :cascade do |t|
     t.string "first_name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20171221013801) do
     t.integer "property_zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
